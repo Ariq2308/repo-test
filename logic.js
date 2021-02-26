084a591627f1e85452220cca82b5403741edcaf9
Ahmed Ariq Assabiq <47692246+Ariq2308@users.noreply.github.com>
2021-02-26 21:38:29 +0700

    initial commit

---
+++ logic.js
@@ -0,0 +1,25 @@
+
+// var nama = 'Budi'; 
+// console.log(nama); 
+
+// console.log("nama saya budi");
+
+// var kelas;
+// kelas = 10;
+// kelas = 11; 
+// kelas = 30;
+
+// console.log(kelas); 
+
+// let menikah = true;
+// console.log(menikah);
+
+// Codingan Prompt
+
+// var nama = prompt("Masukkan Nama")
+
+// console.log(nama)
+
+// alert("Hai, Selamat Datang")
+
+
